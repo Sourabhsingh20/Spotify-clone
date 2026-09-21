49a0c52746abc5c1cf3ad7f3d672288bc2bc1eef
Sourabh Singh <sourabhsingh27436@gmail.com>
2026-09-21 17:32:29 +0530

    All File Uploaded

---
+++ Signup.js
@@ -0,0 +1,20 @@
+let btn=document.querySelector("#signupbutton");
+
+btn.addEventListener("click",function(){
+    window.location.href="SignUpPage.html";
+});
+
+let btn2=document.querySelector("#loginTopButton");
+
+btn2.addEventListener("click",function(){
+    window.location.href="LoginPage.html";
+});
+
+
+let btn3=document.querySelector("#signUpbutton");
+
+btn3.addEventListener("click",function(){
+    window.location.href="SignUpPage.html";
+});
+
+
